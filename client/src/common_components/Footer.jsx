a64e995e86be5dfbef06342d897afb2c1301748a
Ayush Singh <aayush.singh2100@gmail.com>
2024-03-15 23:18:05 +0530

    Modified Footer

--- client/src/common_components/Footer.jsx
+++ client/src/common_components/Footer.jsx
@@ -1,14 +1,18 @@
-import React from 'react'
+import React from "react";
 
 const Footer = () => {
   return (
-    <footer className="p-[20px] mt-[100px] bg-[#dcffff] flex items-center justify-between">
-      <img src={require('../images/lekha-logo.png')} alt="logo" className="h-[50px] bg-transparent"/>
-      <span>
-        Made by Ayush Singh
-      </span>
+    <footer className="py-[20px] mt-[100px] bg-[#dcffff] flex justify-center">
+      <div className="flex justify-between w-[80%] items-center">
+        <img
+          src={require("../images/lekha-logo.png")}
+          alt="logo"
+          className="h-[50px] bg-transparent"
+        />
+        <span>Made by Ayush Singh</span>
+      </div>
     </footer>
-  )
-}
+  );
+};
 
-export default Footer
+export default Footer;
