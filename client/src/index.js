1899ef02d610b3a62432973b0185204e3088f7d0
Ayush Singh <aayush.singh2100@gmail.com>
2024-06-15 11:38:07 +0530

    Persist store added

--- client/src/index.js
+++ client/src/index.js
@@ -3,13 +3,18 @@ import ReactDOM from 'react-dom/client';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import './index.css';
-import { store } from './redux/store.js';
+import { store, persistedStore } from './redux/store.js';
 import { Provider } from 'react-redux';
+import { PersistGate } from 'redux-persist/integration/react'
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
+
 root.render(
   <React.StrictMode>
   <Provider store={store}>
-    <App />
+    <PersistGate loading={null} persistor={persistedStore}>
+      <App />
+    </PersistGate>
   </Provider>
   </React.StrictMode>
 );
--- client/src/redux/store.js
+++ client/src/redux/store.js
@@ -1,9 +1,20 @@
 import { configureStore, combineReducers } from '@reduxjs/toolkit'
 import authSlice from './authSlice'
 import blogSlice from './blogSlice'
+import { persistStore, persistReducer } from 'redux-persist'
+import storage from 'redux-persist/lib/storage'
+
+const persistConfig = {
+    key: "store",
+    storage
+}
 
 const rootReducer = combineReducers({ auth: authSlice, blog: blogSlice })
+const persistedReducer = persistReducer(persistConfig, rootReducer)
+
 
 export const store = configureStore({
-    reducer: rootReducer
+    reducer: persistedReducer
 })
+
+export const persistedStore = persistStore(store)
